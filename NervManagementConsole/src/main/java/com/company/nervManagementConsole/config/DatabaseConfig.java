1ff1813bd108391be35a44561dfb6a7b70cb7cfb
Sergio Ciufo <carminesergio.ciufo@gmail.com>
2024-11-19 11:51:29 +0100

    DatabaseConfig now with Datasource.

--- NervManagementConsole/src/main/java/com/company/nervManagementConsole/config/DatabaseConfig.java
+++ NervManagementConsole/src/main/java/com/company/nervManagementConsole/config/DatabaseConfig.java
@@ -1,11 +1,40 @@
 package com.company.nervManagementConsole.config;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+//import java.sql.DriverManager; non ci serve più
 import java.sql.SQLException;
 
-import com.company.nervManagementConsole.utils.DatabaseCostants;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
+//import com.company.nervManagementConsole.utils.DatabaseCostants; da eliminare
+
+public class DatabaseConfig {
+
+    public static Connection getConnection() throws SQLException {
+    	Context ctx = null;
+    	Connection connection = null;
+        try {
+        	ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("java:/comp/env/jdbc/MyLocalDB");
+
+            connection = ds.getConnection();
+            System.out.println("NERV DATABASE CONNECTION OK!");
+
+        } catch (NamingException e) {
+            System.out.println("Error during DataSource lookup: " + e.getMessage());
+            throw new SQLException("Unable to find DataSource", e);
+        } catch (SQLException e) {
+            System.out.println("Error database connection: " + e.getMessage());
+            throw e;
+        }
+        return connection;
+    }
+}
+
+/* versione senza DataSource
 public class DatabaseConfig {
     public static Connection getConnection() throws SQLException {
         Connection connection = null;
@@ -24,3 +53,4 @@ public class DatabaseConfig {
         return connection;
     }
 }
+*/
